f36f0c50883b6a0fdc936b34a1e3f5c81fb51e05
Shynzyn <49045181+Shynzyn@users.noreply.github.com>
2023-02-10 11:53:21 +0200

    .

--- mysite/todolist/views.py
+++ mysite/todolist/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views import generic
 
-# Create your views here.
+from .models import Task
+
+
+#
+# @login_required
+# def tasks(request):
+#     tasks = Task.objects.filter(user=request.user)
+#     return render(request, 'tasks.html', {'tasks': tasks})
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = 'tasks.html'
+    context_object_name = "tasks"
